a6f8368c043f75d7279c1c69dff0b34dd466c525
shadow_walker <shadowwalkersb@gmail.com>
2023-04-30 19:23:47 -0500

    tests imports

---
+++ recipe/test_imports.py
@@ -0,0 +1,140 @@
+from bisect import insort
+from builtins import object
+from builtins import range
+from collections import Counter
+from collections import MutableMapping
+from collections import defaultdict
+from copy import copy
+from copy import deepcopy
+from distutils.spawn import find_executable
+from functools import reduce
+from future import standard_library
+from future.utils import with_metaclass
+from io import StringIO
+from math import acos
+from math import atan2
+from math import ceil
+from math import log10
+from math import pi
+from math import pow
+from math import sin
+from math import sqrt
+from math import tanh
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.backends.backend_pdf import FigureCanvasPdf
+from matplotlib.figure import Figure
+from matplotlib.ticker import MaxNLocator
+from mpl_toolkits.mplot3d import Axes3D
+from multiprocessing import Array
+from multiprocessing import Pool
+from numpy import arange
+from numpy import array
+from operator import itemgetter
+from optparse import OptionParser
+from os import path
+from os import remove
+from os import system
+from os import unlink
+from past.utils import old_div
+from pickle import dump
+from pickle import dumps
+from pickle import load
+from pickle import loads
+from pprint import pprint
+from pylab import colorbar
+from pylab import figure
+from pylab import scatter
+from pylab import show
+from pylab import subplot
+from queue import Queue
+from random import choice
+from random import uniform
+from scipy import ndimage
+from scipy import optimize
+from scipy import sparse
+from scipy.interpolate import interp1d
+from scipy.optimize import minimize
+from scipy.signal import argrelextrema
+from scipy.signal import find_peaks_cwt
+from scipy.stats import norm
+from shutil import copy2
+from shutil import copyfile
+from sklearn import linear_model
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from struct import pack
+from struct import unpack
+from sys import argv
+from sys import exit
+from sys import stdout
+from time import ctime
+from time import gmtime
+from time import localtime
+from time import mktime
+from time import sleep
+from time import strftime
+from time import strptime
+from time import time
+from weakref import WeakKeyDictionary
+from xml.sax.handler import ContentHandler
+from zlib import compress
+from zlib import decompress
+import _thread
+import abc
+import argparse
+import atexit
+import base64
+import collections
+import colorsys
+import copy
+import datetime
+import fnmatch
+import gc
+import getpass
+import glob
+import importlib
+import itertools
+import json
+import math
+import matplotlib
+import matplotlib.cm
+import matplotlib.mlab
+import matplotlib.pyplot
+import multiprocessing
+import numpy
+import numpy.linalg
+import operator
+import os
+import os.path
+import pickle
+import platform
+import pprint
+import pylab
+import queue
+import random
+import re
+import scipy
+import scipy.ndimage
+import scipy.spatial.distance
+import select
+import shelve
+import shutil
+import signal
+import sklearn.decomposition
+import socket
+import string
+import struct
+import subprocess
+import sys
+import tempfile
+import tensorflow
+import tensorflow.keras.models
+import threading
+import time
+import traceback
+import warnings
+import weakref
+import webbrowser
+import xml.etree.ElementTree
+import xml.sax
+import zlib
