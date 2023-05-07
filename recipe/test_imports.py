9141d2a3ac035bf934580aed399d1ec6e7a09ee6
shadow_walker <shadowwalkersb@gmail.com>
2023-05-07 18:28:35 -0500

    [ci skip] Revert "tests copy from cryoem/eman2, these are actually already run, because they are on cryoem/eman2, why duplicate here ???"
    119410a49f9abfe44bb8b18b8d4f069d9ee109da

--- recipe/test_imports.py
+++ recipe/test_imports.py
@@ -1,86 +1,140 @@
-from libpyAligner2 import Aligners
-from libpyAligner2 import Ctf
-from libpyAligner2 import EMAN1Ctf
-from libpyAligner2 import EMAN2Ctf
-from libpyAligner2 import __Aligner
-from libpyAnalyzer2 import Analyzers
-from libpyAnalyzer2 import __Analyzer
-from libpyAverager2 import Averagers
-from libpyAverager2 import __Averager
-from libpyBoxingTools2 import BoxingTools
-from libpyCmp2 import Cmps
-from libpyCmp2 import Log
-from libpyCmp2 import XYData
-from libpyCmp2 import __Cmp
-from libpyEMData2 import EMData
-from libpyEMObject2 import TypeDict
-from libpyFundamentals2 import fp_flag
-from libpyFundamentals2 import kernel_shape
-from libpyFundamentals2 import morph_type
-from libpyGLUtils2 import EMFTGL
-from libpyGLUtils2 import FTGLFontMode
-from libpyGLUtils2 import GLUtil
-from libpyGeometry2 import Pixel
-from libpyGeometry2 import Region
-from libpyMarchingCubes2 import Isosurface
-from libpyMarchingCubes2 import MarchingCubes
-from libpyPDBReader2 import PDBReader
-from libpyPointArray2 import PointArray
-from libpyPolarData2 import PolarData
-from libpyPolarData2 import UnevenMatrix
-from libpyProcessor2 import Processor
-from libpyProcessor2 import Processors
-from libpyProjector2 import Projectors
-from libpyProjector2 import __Projector
-from libpyReconstructor2 import Reconstructors
-from libpyReconstructor2 import __Reconstructor
-
+from bisect import insort
+from builtins import object
+from builtins import range
+from collections import Counter
+from collections import MutableMapping
+from collections import defaultdict
+from copy import copy
+from copy import deepcopy
+from distutils.spawn import find_executable
+from functools import reduce
+from future import standard_library
+from future.utils import with_metaclass
+from io import StringIO
+from math import acos
+from math import atan2
+from math import ceil
+from math import log10
+from math import pi
+from math import pow
+from math import sin
+from math import sqrt
+from math import tanh
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.backends.backend_pdf import FigureCanvasPdf
+from matplotlib.figure import Figure
+from matplotlib.ticker import MaxNLocator
+from mpl_toolkits.mplot3d import Axes3D
+from multiprocessing import Array
+from multiprocessing import Pool
+from numpy import arange
+from numpy import array
+from operator import itemgetter
+from optparse import OptionParser
+from os import path
+from os import remove
+from os import system
+from os import unlink
+from past.utils import old_div
+from pickle import dump
+from pickle import dumps
+from pickle import load
+from pickle import loads
+from pprint import pprint
+from pylab import colorbar
+from pylab import figure
+from pylab import scatter
+from pylab import show
+from pylab import subplot
+from queue import Queue
+from random import choice
+from random import uniform
+from scipy import ndimage
+from scipy import optimize
+from scipy import sparse
+from scipy.interpolate import interp1d
+from scipy.optimize import minimize
+from scipy.signal import argrelextrema
+from scipy.signal import find_peaks_cwt
+from scipy.stats import norm
+from shutil import copy2
+from shutil import copyfile
+from sklearn import linear_model
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from struct import pack
+from struct import unpack
+from sys import argv
+from sys import exit
+from sys import stdout
+from time import ctime
+from time import gmtime
+from time import localtime
+from time import mktime
+from time import sleep
+from time import strftime
+from time import strptime
+from time import time
+from weakref import WeakKeyDictionary
+from xml.sax.handler import ContentHandler
+from zlib import compress
+from zlib import decompress
+import _thread
+import abc
+import argparse
+import atexit
+import base64
+import collections
+import colorsys
+import copy
+import datetime
+import fnmatch
+import gc
+import getpass
+import glob
+import importlib
+import itertools
+import json
+import math
+import matplotlib
+import matplotlib.cm
+import matplotlib.mlab
+import matplotlib.pyplot
+import multiprocessing
+import numpy
+import numpy.linalg
+import operator
+import os
+import os.path
+import pickle
 import platform
-if platform.system() != "Windows":
-    from libpyTomoSeg2 import TomoSeg
-
-from libpyTransform2 import OrientGens
-from libpyTransform2 import Quaternion
-from libpyTransform2 import Symmetries
-from libpyTransform2 import Symmetry3D
-from libpyTransform2 import Transform
-from libpyTransform2 import Vec2f
-from libpyTransform2 import Vec3f
-from libpyTransform2 import Vec3i
-from libpyTransform2 import Vec4f
-from libpyTransform2 import __OrientationGenerator
-from libpyTypeConverter2 import EMNumPy
-from libpyUtils2 import EMUtil
-from libpyUtils2 import ImageSort
-from libpyUtils2 import TestUtil
-from libpyUtils2 import Util
-
-import libpyUtils2
-libpyUtils2.Util.FakeKaiserBessel
-libpyUtils2.Util.KaiserBessel
-libpyUtils2.Util.KaiserBessel.kbi0_win
-libpyUtils2.Util.KaiserBessel.kbsinh_win
-libpyUtils2.Util.sincBlackman
-libpyUtils2.EMUtil.EMDataType
-libpyUtils2.EMUtil.ImageType
-libpyUtils2.Util.Gaussian
-libpyUtils2.Util.tmpstruct
-
-import libpyAligner2
-libpyAligner2.Ctf.CtfType
-
-import libpyBoxingTools2
-libpyBoxingTools2.BoxingTools.CmpMode
-
-import libpyCmp2
-libpyCmp2.Log.LogLevel
-libpyCmp2.XYData.Pair
-
-import libpyEMData2
-libpyEMData2.EMData
-
-import libpyPointArray2
-libpyPointArray2.PointArray.Density2PointsArrayAlgorithm
-
-import libpyProcessor2
-libpyProcessor2.Processor.fourier_filter_types
+import pprint
+import pylab
+import queue
+import random
+import re
+import scipy
+import scipy.ndimage
+import scipy.spatial.distance
+import select
+import shelve
+import shutil
+import signal
+import sklearn.decomposition
+import socket
+import string
+import struct
+import subprocess
+import sys
+import tempfile
+import tensorflow
+import tensorflow.keras.models
+import threading
+import time
+import traceback
+import warnings
+import weakref
+import webbrowser
+import xml.etree.ElementTree
+import xml.sax
+import zlib
