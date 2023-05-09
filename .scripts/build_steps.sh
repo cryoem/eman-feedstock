e4fa9a833e58479a9975d59b54174cff8dbd2eac
shadow_walker <shadowwalkersb@gmail.com>
2023-05-09 17:36:13 -0500

    MNT: Re-rendered with conda-build 3.24.0, conda-smithy 3.23.1, and conda-forge-pinning 2023.05.08.17.24.41

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -47,7 +47,7 @@ source run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/sudo -n yum install -y mesa-libGL mesa-libGLU-devel
+/usr/bin/sudo -n yum install -y mesa-libGL
 
 
 # make the build number clobber
